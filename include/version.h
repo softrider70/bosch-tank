2c277fbdfaf71f5a50bda63bb44373252e9b92d0
ESP32 Developer <developer@local>
2026-06-01 18:13:23 +0200

    chore: build #84

--- include/version.h
+++ include/version.h
@@ -12,9 +12,9 @@
 
 #define VERSION_MAJOR               0
 #define VERSION_MINOR               1
-#define BUILD_NUMBER                83
-#define BUILD_TIMESTAMP             "2026-06-01 16:28:32"
-#define VERSION_STRING              "v0.1.83"
+#define BUILD_NUMBER                84
+#define BUILD_TIMESTAMP             "2026-06-01 18:12:27"
+#define VERSION_STRING              "v0.1.84"
 
 // Full version identifier for logs and UI
 #define APP_FULL_VERSION            VERSION_STRING " (" BUILD_TIMESTAMP ")"
